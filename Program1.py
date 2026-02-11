e2af4394bd674562f379a49f7bc5055a20cc8201
yashkulkarni-git <imkulkarni2003@gmail.com>
2026-02-11 15:21:20 +0530

    Delete Program1.py

--- Program1.py
+++
@@ -1,9 +0,0 @@
-#Write a program to input p, r, n and find out interest using simple input
-
-p=int(input("enter P"))
-r=int(input("enter R"))
-n=int(input("enter N"))
-
-int=p*r*n/100
-
-print("Simple Interest is ",int)
